72e30b007648988dcd7dcc60f2190638dbea1563
AntoineGuillo55 <guillo.antoine93@gmail.com>
2024-07-09 13:13:20 +0200

    creation dossier script avec script creation DB

---
+++ scripts/create_db_enchere_mariadb.sql
@@ -0,0 +1,86 @@
+-- Script de création de la base de données ENCHERES
+--   type :      MariaDB
+--
+
+CREATE TABLE CATEGORIES (
+    no_categorie   INT AUTO_INCREMENT NOT NULL,
+    libelle        VARCHAR(30) NOT NULL,
+    PRIMARY KEY (no_categorie)
+);
+
+CREATE TABLE ENCHERES (
+    no_utilisateur   INT NOT NULL,
+    no_article       INT NOT NULL,
+    date_enchere     DATETIME NOT NULL,
+    montant_enchere  INT NOT NULL,
+    PRIMARY KEY (no_utilisateur, no_article)
+);
+
+CREATE TABLE RETRAITS (
+    no_article         INT NOT NULL,
+    rue                VARCHAR(30) NOT NULL,
+    code_postal        VARCHAR(15) NOT NULL,
+    ville              VARCHAR(30) NOT NULL,
+    PRIMARY KEY (no_article)
+);
+
+CREATE TABLE UTILISATEURS (
+    no_utilisateur   INT AUTO_INCREMENT NOT NULL,
+    pseudo           VARCHAR(30) NOT NULL,
+    nom              VARCHAR(30) NOT NULL,
+    prenom           VARCHAR(30) NOT NULL,
+    email            VARCHAR(50) NOT NULL, -- Augmenté la taille de l'email
+    telephone        VARCHAR(15),
+    rue              VARCHAR(30) NOT NULL,
+    code_postal      VARCHAR(10) NOT NULL,
+    ville            VARCHAR(30) NOT NULL,
+    mot_de_passe     VARCHAR(30) NOT NULL,
+    credit           INT NOT NULL,
+    administrateur   BOOLEAN NOT NULL,
+    PRIMARY KEY (no_utilisateur)
+);
+
+CREATE TABLE ARTICLES_VENDUS (
+    no_article                    INT AUTO_INCREMENT NOT NULL,
+    nom_article                   VARCHAR(30) NOT NULL,
+    description                   VARCHAR(300) NOT NULL,
+    date_debut_encheres           DATE NOT NULL,
+    date_fin_encheres             DATE NOT NULL,
+    prix_initial                  INT,
+    prix_vente                    INT,
+    no_utilisateur                INT NOT NULL,
+    no_categorie                  INT NOT NULL,
+    PRIMARY KEY (no_article)
+);
+
+CREATE table ROLES (
+	no_role		INT auto_increment not null,
+	email		VARCHAR(50) not null,
+	role		VARCHAR(30) not null,
+	primary key (no_role)
+);
+
+ALTER TABLE ARTICLES_VENDUS
+    ADD CONSTRAINT encheres_utilisateur_fk FOREIGN KEY (no_utilisateur) REFERENCES UTILISATEURS (no_utilisateur)
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION;
+
+ALTER TABLE ENCHERES
+    ADD CONSTRAINT encheres_articles_vendus_fk FOREIGN KEY (no_article) REFERENCES ARTICLES_VENDUS (no_article)
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION;
+
+ALTER TABLE RETRAITS
+    ADD CONSTRAINT retraits_articles_vendus_fk FOREIGN KEY (no_article) REFERENCES ARTICLES_VENDUS (no_article)
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION;
+
+ALTER TABLE ARTICLES_VENDUS
+    ADD CONSTRAINT articles_vendus_categories_fk FOREIGN KEY (no_categorie) REFERENCES CATEGORIES (no_categorie)
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION;
+
+ALTER TABLE ARTICLES_VENDUS
+    ADD CONSTRAINT ventes_utilisateur_fk FOREIGN KEY (no_utilisateur) REFERENCES UTILISATEURS (no_utilisateur)
+    ON DELETE NO ACTION 
+    ON UPDATE NO ACTION;
