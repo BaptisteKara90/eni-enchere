56473a66dfdffd3cc6d7f2d2137a61eb93089155
unknown <Administrateur@51SE27-H95HHZ3.ad.campus-eni.fr>
2024-07-18 12:09:37 +0200

    enchere mieux

--- src/main/java/fr/eni/ecole/enchere/controllers/EncheresController.java
+++ src/main/java/fr/eni/ecole/enchere/controllers/EncheresController.java
@@ -34,37 +34,56 @@ public class EncheresController {
 
     @PostMapping("/add-enchere")
     public String addEnchere(@ModelAttribute("enchere") Enchere enchere, RedirectAttributes redirectAttributes) {
-     Utilisateur utilisateur = utilisateurService.getUtilisateur(enchere.getNo_utilisateur());
-     ArticleVendu article = articleVenduService.getArticleVendu(enchere.getNo_article());
+        // Récupérer l'utilisateur actuellement connecté
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Utilisateur currentUtilisateur = (Utilisateur) auth.getPrincipal();
-        currentUtilisateur = utilisateurService.getUtilisateur(utilisateur.getNo_utilisateur());
 
-     if (utilisateur.getCredit() > enchere.getMontant_enchere()){
-         enchere.setDate_enchere(LocalDate.now());
-         enchereService.addEnchere(enchere);
-         utilisateur.setCredit(utilisateur.getCredit() - enchere.getMontant_enchere());
-         utilisateurService.changeCredit(utilisateur.getNo_utilisateur(), utilisateur.getCredit());
-         List<Enchere> enchereArticle = enchereService.getEnchereByArticle(enchere.getNo_article());
+        // Récupérer l'utilisateur et l'article liés à l'enchère
+        Utilisateur utilisateur = utilisateurService.getUtilisateur(enchere.getNo_utilisateur());
+        ArticleVendu article = articleVenduService.getArticleVendu(enchere.getNo_article());
 
-         if (enchereArticle.size() > 1){
-             enchereArticle.removeLast();
-             Enchere lastEnchere = enchereArticle.getLast();
-             Utilisateur refundUtilisateur = utilisateurService.getUtilisateur(lastEnchere.getNo_utilisateur());
-             refundUtilisateur.setCredit(lastEnchere.getMontant_enchere() + refundUtilisateur.getCredit());
-             utilisateurService.changeCredit(refundUtilisateur.getNo_utilisateur(), refundUtilisateur.getCredit());
-         }
-     } else if(utilisateur.getCredit() <= enchere.getMontant_enchere()) {
-         String erreur = "Vous avez pas les moyens pour acquérir cette objet !";
-         redirectAttributes.addFlashAttribute("erreur", erreur);
-         return "redirect:/article?id=" + article.getNo_article();
-     }
-     if(currentUtilisateur.getNo_utilisateur() == article.getUtilisateur().getNo_utilisateur()){
-         String erreur = "Vous ne pouvez pas enchérir votre propre objet ! (petit(e) coquin(e) va!)";
-         redirectAttributes.addFlashAttribute("erreur", erreur);
-         return "redirect:/article?id=" + article.getNo_article();
-     }
+        // Vérifier si l'utilisateur est le propriétaire de l'article
+        if (currentUtilisateur.getNo_utilisateur() == article.getUtilisateur().getNo_utilisateur()) {
+            String erreur = "Vous ne pouvez pas enchérir sur votre propre objet ! (petit(e) coquin(e) va!)";
+            redirectAttributes.addFlashAttribute("erreur", erreur);
+            return "redirect:/article?id=" + article.getNo_article();
+        }
 
-     return "redirect:/article?id=" + article.getNo_article();
+        // Vérifier si l'utilisateur a suffisamment de crédit pour l'enchère
+        if (utilisateur.getCredit() >= enchere.getMontant_enchere()) {
+            // Vérifier si l'enchère est supérieure à l'enchère actuelle
+            List<Enchere> enchereArticle = enchereService.getEnchereByArticle(enchere.getNo_article());
+            if (!enchereArticle.isEmpty()) {
+                Enchere derniereEnchere = enchereArticle.get(enchereArticle.size() - 1);
+                if (enchere.getMontant_enchere() <= derniereEnchere.getMontant_enchere()) {
+                    String erreur = "Vous devez enchérir plus que l'enchère actuelle !";
+                    redirectAttributes.addFlashAttribute("erreur", erreur);
+                    return "redirect:/article?id=" + article.getNo_article();
+                }
+            }
+
+            // Ajouter la date de l'enchère et enregistrer l'enchère
+            enchere.setDate_enchere(LocalDate.now());
+            enchereService.addEnchere(enchere);
+
+            // Mettre à jour le crédit de l'utilisateur enchérisseur
+            utilisateur.setCredit(utilisateur.getCredit() - enchere.getMontant_enchere());
+            utilisateurService.changeCredit(utilisateur.getNo_utilisateur(), utilisateur.getCredit());
+
+            // Rembourser la dernière enchère perdante
+            if (enchereArticle.size() > 1) {
+                Enchere derniereEncherePerdante = enchereArticle.get(enchereArticle.size() - 2);
+                Utilisateur utilisateurRembourse = utilisateurService.getUtilisateur(derniereEncherePerdante.getNo_utilisateur());
+                utilisateurRembourse.setCredit(utilisateurRembourse.getCredit() + derniereEncherePerdante.getMontant_enchere());
+                utilisateurService.changeCredit(utilisateurRembourse.getNo_utilisateur(), utilisateurRembourse.getCredit());
+            }
+        } else {
+            // Si l'utilisateur n'a pas suffisamment de crédit, rediriger avec un message d'erreur
+            String erreur = "Vous n'avez pas les moyens d'acquérir cet objet !";
+            redirectAttributes.addFlashAttribute("erreur", erreur);
+        }
+
+        // Rediriger vers la page de l'article après le traitement de l'enchère
+        return "redirect:/article?id=" + article.getNo_article();
     }
 }
