40fd0be1f197a5c988af9015fc584c9498ea5583
AntoineGuillo55 <guillo.antoine93@gmail.com>
2024-07-19 11:32:22 +0200

    accès page admin uniquement si admin

--- src/main/java/fr/eni/ecole/enchere/controllers/AdminController.java
+++ src/main/java/fr/eni/ecole/enchere/controllers/AdminController.java
@@ -5,6 +5,8 @@ import fr.eni.ecole.enchere.bll.CategorieService;
 import fr.eni.ecole.enchere.bll.UtilisateurService;
 import fr.eni.ecole.enchere.bo.Categorie;
 import fr.eni.ecole.enchere.bo.Utilisateur;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,12 +27,20 @@ public class AdminController {
     @GetMapping("/admin")
     public String admin(Model model) {
 
-        List<Utilisateur> utilisateurs = utilisateurService.getUtilisateurs();
-        List<Categorie> categories = categorieService.getAllCategories();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Utilisateur utilisateur = (Utilisateur) auth.getPrincipal();
 
-        model.addAttribute("utilisateurs", utilisateurs);
-        model.addAttribute("categories", categories);
+        if(utilisateur.isAdministrateur()) {
+            List<Utilisateur> utilisateurs = utilisateurService.getUtilisateurs();
+            List<Categorie> categories = categorieService.getAllCategories();
 
-        return "administrator";
+            model.addAttribute("utilisateurs", utilisateurs);
+            model.addAttribute("categories", categories);
+
+            return "administrator";
+        }
+
+        return "redirect:/";
+
     }
 }
